681d8c83da2fe3e8ea147cec8f37bb330841c7be
hmh1842 <hmh1842@rit.edu>
2025-07-04 05:03:12 -0400

    no work

--- database/tables.sql
+++ database/tables.sql
@@ -5,44 +5,71 @@ DROP TABLE IF EXISTS Platform;
 DROP TABLE IF EXISTS Person;
 DROP TABLE IF EXISTS Studio;
 DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Movie_Genre;
+DROP TABLE IF EXISTS Movie_Studio;
+DROP TABLE IF EXISTS Movie_Platform;
+DROP TABLE IF EXISTS Movie_Person;
 
 CREATE TABLE MOVIES(
     MovieID         int PRIMARY KEY,
+    RelasedDate     VARCHAR(50),
     Length          int,
     Title           VARCHAR(50),
-    MPAARating      varchar(2),
+    MMPA            varchar(2)
 );
 
 CREATE TABLE USERS(
-    FirstName       VARCHAR(20),
-    LastName        VARCHAR(20),
-    Username        VARCHAR(20) PRIMARY KEY UNIQUE,
-    Password        VARCHAR(20) NOT NULL,
+    FirstName       VARCHAR(50),
+    LastName        VARCHAR(50),
+    Username        VARCHAR(50) PRIMARY KEY UNIQUE,
+    Password        VARCHAR(50) NOT NULL,
     LastAccessed    DATE
 );
 
 CREATE TABLE Collection(
-    CollectionID    int PRIMARY KEY,
-    Username        VARCHAR(20) NOT NULL,
-    CollectionName  VARCHAR(20)
+    CollectionID    int PRIMARY KEY ,
+    Username        VARCHAR(50) NOT NULL,
+    CollectionName  VARCHAR(50)
 );
 
 CREATE TABLE Platform(
     PlatformID      int PRIMARY KEY,
-    PlatformName    VARCHAR(20) NOT NULL
+    PlatformName    VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Person(
     PersonID        int PRIMARY KEY,
-    Name            VARCHAR(20) NOT NULL
-)
+    Name            VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE Studio(
     StudioID        int PRIMARY KEY,
-    Name            VARCHAR(20) NOT NULL,
-)
+    Name            VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE Genre(
     GenreID         int PRIMARY KEY,
-    Name            VARCHAR(20) NOT NULL,
-)
+    Name            VARCHAR(50) NOT NULL
+);
+
+-- RELATIONSHIP TABLES
+
+CREATE TABLE Movie_Genre(
+    MovieID         int NOT NULL,
+    GenreID         int NOT NULL
+);
+
+CREATE TABLE Movie_Studio(
+    MovieID         int NOT NULL,
+    StudioID        int NOT NULL
+);
+
+CREATE TABLE Movie_Platform(
+    MovieID         int NOT NULL,
+    GenreID         int NOT NULL
+);
+
+CREATE TABLE Movie_Person(
+    MovieID         int NOT NULL,
+    GenreID         int NOT NULL
+);
--- src/run_sql_tables.py
+++ src/run_sql_tables.py
@@ -3,7 +3,7 @@ from sshtunnel import SSHTunnelForwarder
 import os
 import psycopg2
 
-def connect_and_setup():
+def connect_to_db():
     load_dotenv()
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
@@ -16,7 +16,7 @@ def connect_and_setup():
     )
     ssh_server.start()
 
-    #Connect to SQL server
+    # Connect to SQL server
     conn = psycopg2.connect(
         database='csci320_movies',
         user=username,
@@ -25,6 +25,11 @@ def connect_and_setup():
         port=ssh_server.local_bind_port,
     )
     cursor = conn.cursor()
+    return conn, cursor
+
+def connect_and_setup():
+
+    conn, cursor = connect_to_db()
 
     with open('../database/tables.sql', 'r') as file:
         sql_script = file.read()
@@ -39,6 +44,56 @@ def connect_and_setup():
     cursor.close()
     conn.close()
 
+def insert_movie(movieID, title, runtime, mmpa, releasedDate):
+    conn, cursor = connect_to_db()
+    print(movieID)
+    query = f"INSERT INTO movies (MovieID, Title, Length, MMPA, ReleasedDate) VALUES ({movieID}, {title}, {runtime}, {mmpa}, {releasedDate})"
+    cursor.execute(query)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+def insert_two_values(table, var1, var2, value1, value2):
+    conn, cursor = connect_to_db()
+    query = f"INSERT INTO {table} ({var1}, {var2}) VALUES ({value1}, {value2})"
+    cursor.execute(query)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+def mass_movie_insert():
+    with open('../data/tmdb_5000_movies.csv', 'r') as file:
+        sql_script = file.read()
+
+        for line in sql_script.split('\n')[1:]:
+            splitted = line.split(',')
+            print(line)
+            movieID = splitted[1]
+            title = splitted[6]
+            runtime = splitted[5]
+            release_date = splitted[4]
+            platform = splitted[-1]
+            mmpa = splitted[-2]
+            genre = splitted[0] #THIS IS A LIST OF DICTIONARY {ID:0 , Name'adventure'}
+            studio = splitted[3]    ##LIST OF studios {name: 'FOX', id:0}
+
+            insert_movie(movieID, title, runtime, mmpa, release_date)
+            for genreID, genreName in genre:
+                insert_two_values('Genre', 'GenreID', 'Name', genreID, genreName)
+                insert_two_values('Movie_Genre', 'MovieID', 'GenreID', movieID, genreID)
+            for studioName, studioID in studio:
+                insert_two_values('Studio', 'StudioID', 'Name', studioID, studioName)
+                insert_two_values('Movie_Studio', 'MovieID', 'StudioID', movieID, studioID)
+            insert_two_values('Platform', 'PlatformID', 'Name', platform['id'], platform['name'])
+            insert_two_values('Movie_Platform', 'MovieID', 'PlatformID', movieID, platform['id'])
+
+
+
+
+
 
 if __name__ == "__main__":
     connect_and_setup()
+    mass_movie_insert()
